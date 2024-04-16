df27362ecdca20ad57cbbc3549602cbc9e3abd22
Toby Pham <148511105+TobyKenobi212@users.noreply.github.com>
2024-04-15 17:46:29 -0700

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in Python and C/C++ these days.
+      I like programming and taking the time to learn new skills to apply to my career and day to day tasks.
+      Here is my <a href="https://github.com/" target="_blank">Github</a>!`,
+    "cs-degree": `This is where I will place my B.S. Computer Engineering degree when I graduate in Spring of 2026.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch movies and TV shows. 
+    I also like to relax at my PC where I would play games with my friends.`,
+    tv: `That's my TV. My favorite movies are Star Wars and The Lighthouse.`,
+    bed: `This is where I sleep. Great ideas comes when I'm lying on my bed and I let my thoughts dwell throughout my mind.`,
+    resume: `This is my desk and on it is my resume. <a href="https://github.com/TobyKenobi212/resume/blob/main/Toby_Pham_Resume.pdf" target="_blank">Check it out?</a>
+    Contact me at tobypham03@gmail.com if you have any interesting opportunities!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+    library: `There are various books besides progamming on my shelf. I am an advent learner and enjoy spending time to learn various topics
+    which include knowledge outside of my field.`,
+    exit: `If you want to exit Toby's portfolio, just close the tab.`,
+  };
